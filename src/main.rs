9a47b3bcfddc22cc5df8d6e48d3f938c77945d35
Matte <mattias.johansson@funnel.io>
2023-07-01 15:42:19 +0200

    Simplified the code, also using io::copy after skipping head lines

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{io, io::prelude::*};
 use std::collections::VecDeque;
-use std::io::BufWriter;
+use std::io::{BufWriter, StdoutLock};
+use std::result::Result as is;
 
 use clap::Parser;
 
@@ -10,27 +11,38 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Parser)]
 struct Args {
     #[clap(default_value_t = false, short, long)]
+    debug: bool,
+    #[clap(default_value_t = false, short, long)]
     tail: bool,
     #[clap(default_value_t = 10)]
     n: usize,
+}
+
+fn drop_head(num_lines: usize, out: &mut BufWriter<StdoutLock>) -> std::io::Result<()> {
+    let _ = io::stdin().lines().skip(num_lines);
+    let _ = io::copy(&mut io::stdin().lock(), out);
+    Ok(())
+}
+
+fn drop_tail(num_lines: usize, out: &mut BufWriter<StdoutLock>) -> Result<()> {
+    let mut queue = VecDeque::<String>::new();
+    let lines = io::stdin().lines();
+    queue.extend(lines.take(num_lines).filter_map(|result| result.ok()));
+    for line in io::stdin().lines() {
+        queue.push_back(line?);
+        let output = queue.pop_front().expect("Expected line in queue");
+        writeln!(out, "{}", output)?;
+    }
+    Ok(())
 }
 
 pub fn main() -> Result<()> {
     let args = Args::parse();
     let mut out = BufWriter::new(io::stdout().lock());
     if args.tail {
-        let mut queue = VecDeque::<String>::new();
-        let lines = io::stdin().lines();
-        queue.extend(lines.take(args.n).filter_map(|line| line.ok()));
-        for line in io::stdin().lines() {
-            queue.push_back(line?);
-            let output = queue.pop_front().expect("Expected line in queue");
-            writeln!(out, "{}", output)?
-        }
+        drop_tail(args.n, &mut out)?;
     } else {
-        for line in io::stdin().lines().skip(args.n) {
-            writeln!(out, "{}", line?)?
-        }
+        drop_head(args.n, &mut out)?;
     };
     Ok(())
 }
